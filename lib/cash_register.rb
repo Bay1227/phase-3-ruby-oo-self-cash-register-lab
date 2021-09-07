d269a8ccef8f116df64fdc070370ee2cea7b2f6a
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-09-06 23:26:31 -0700

    Cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,73 @@
+# require "pry"
 
+
+#   def add_item(item, price, quantity=1)
+#     self.total += (price * quantity)
+#   end 
+
+#   def items
+#     new_register.items
+#   end 
+
+#   def void_last_transaction
+#   end 
+
+
+# end 
+
+
+class CashRegister
+
+  attr_accessor :total, :discount, :last_transaction
+
+  def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @items = []
+      @last_transaction = nil
+      @last_transaction_name = nil
+  end
+
+  def add_item(title, price, quantity = 1)
+      i = 0
+      while i < quantity do
+          @items << title
+          @last_transaction = price * quantity
+          @last_transaction_name = title
+          i += 1
+      end
+      self.total += price * quantity
+  end
+
+  def apply_discount
+      if self.discount == 0
+          "There is no discount to apply."
+      else
+          self.total -= (self.total * @discount / 100)
+          "After the discount, the total comes to $#{self.total}."
+      end
+  end
+
+  def items
+      @items
+  end
+
+  def void_last_transaction
+      items.delete(@last_transaction_name)
+      if items.empty?
+          self.total = 0.0
+      else
+          self.total -= @last_transaction
+      end
+  end
+
+
+
+
+
+
+
+
+
+
+end
